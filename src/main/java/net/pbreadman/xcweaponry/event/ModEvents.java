1f4aa9eb9f2e1fa76cc86de2f4721c44a914251c
ShockFox05 <dakotastodghill@gmail.com>
2026-09-15 00:08:38 -0400

    base

--- src/main/java/net/pbreadman/xcweaponry/event/ModEvents.java
+++ src/main/java/net/pbreadman/xcweaponry/event/ModEvents.java
@@ -9,7 +9,6 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.pbreadman.xcweaponry.items.ModItems;
 
-
 public class ModEvents {
     @SubscribeEvent
     public static void OntosUse(PlayerInteractEvent.RightClickItem event) {
